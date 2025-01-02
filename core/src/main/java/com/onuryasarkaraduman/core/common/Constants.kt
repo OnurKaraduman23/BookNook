d1de65f2a820733f6271a95b61e87925e8c1fbf3
OnurKaraduman <onury.karaduman@gmail.com>
2025-01-03 00:42:28 +0300

    Added Resource sealed class  And Constants Object

---
+++ core/src/main/java/com/onuryasarkaraduman/core/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.onuryasarkaraduman.core.common
+
+object Constants {
+
+    const val BASE_URL = "https://www.googleapis.com/books/v1/"
+    const val API_KEY = "AIzaSyAS0rskkHYoltc-F4OnsBMitjDgLnktjEY"
+}
