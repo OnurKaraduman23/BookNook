12ce5a4621be7fb5d542a366ed9c29962312c019
OnurKaraduman <onury.karaduman@gmail.com>
2025-01-03 00:44:15 +0300

    Added Home Mapper

---
+++ features/home/data/src/main/java/com/onuryasarkaraduman/mapper/HomeMapper.kt
@@ -0,0 +1,14 @@
+package com.onuryasarkaraduman.mapper
+
+import com.onuryasarkaraduman.dto.HomeCategoriesResponse
+import com.onuryasarkaraduman.model.CategoriesRecommendedModel
+
+fun HomeCategoriesResponse.toDomain(): List<CategoriesRecommendedModel> {
+    return this.items.map { item ->
+        CategoriesRecommendedModel(
+            id = item?.id.orEmpty(),
+            bookName = item?.volumeInfo?.title.orEmpty(),
+            bookUrl = item?.volumeInfo?.imageLinks?.thumbnail.orEmpty()
+        )
+    }
+}
